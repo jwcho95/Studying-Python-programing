1e7fbdca6365ee066aef4ef6b48e019ce2a18f0a
jwcho95 <kyaleee@naver.com>
2019-09-02 10:32:44 +0900

    최종수정날짜: 2019/09/02

--- chapter 2.py
+++ chapter 2.py
@@ -81,3 +81,51 @@ int_b = int(string_b)
 print("문자열 자료:", string_a + string_b)
 print("숫자 자료:", int_a + int_b)
 
+
+# <혼자 공부하는 파이썬> 챕터 2-4
+
+# format 함수로 숫자를 문자열로 변환하기
+
+string_c = "{}".format(10)
+print(string_c)
+print(type(string_c))
+
+# format() 함수의 다양한 형태
+
+format_a = "{}만원".format(5000)
+format_b = "파이썬 열공하여 첫 연봉 {}만 원 만들기".format(5000)
+format_c = "{} {} {}".format(3000, 4000, 5000)
+format_d = "{} {} {}".format(1, "문자열", True)
+
+print(format_a)
+print(format_b)
+print(format_c)
+print(format_d)
+
+# 정수를 특정 칸에 출력하기
+output_a = "{:d}".format(52)
+output_b = "{:5d}".format(52)
+output_c = "{:10d}".format(52)
+output_d = "{:05d}".format(52)
+output_e = "{:05d}".format(-52)
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+print(output_e)
+
+# 기호 붙여 출력하기
+output_f = "{:+d}".format(52)
+output_g = "{:+d}".format(-52)
+output_h = "{: d}".format(52)
+output_i = "{: d}".format(-52)
+
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
+
+# 조합해보기
+output_j = "{:=+5d}".format(52)
+output_k = "{:=+5d}".format(-52)

9efcf9e8228afe8f432c259ffb5829fa5029709a
jwcho95 <kyaleee@naver.com>
2019-09-08 23:33:23 +0900

    최종수정날짜: 2019/09/08

--- chapter 3.py
+++ chapter 3.py
@@ -166,3 +166,28 @@ elif 0 <= score < 0.5:
     print("플랑크톤")
 elif score == 0:
     print("시대를 앞서가는 혁명의 씨앗") 
+
+
+# 유머를 조건문으로 구현하기(2)
+score = float(input("학점 입력> "))
+
+if score == 4.5:
+    print("신")
+elif 4.2 <= score:
+    print("교수님의 사랑")
+elif 3.5 <= score:
+    print("현 체제의 수호자")
+elif 2.8 <= score:
+    print("일반인")
+elif 2.3 <= score:
+    print("일탈을 꿈꾸는 소시민")
+elif 1.75 <= score:
+    print("오락문화의 선구자")
+elif 1.0 <= score:
+    print("불가촉천민")
+elif 0.5 <= score:
+    print("자벌레")
+elif 0 < score:
+    print("플랑크톤")
+else:
+    print("시대를 앞서가는 혁명의 씨앗") # 위 조건문과 다른 점은 이 조건문에는 이미 한번 비교하였던 것은 제외하고 있다는 점이다.

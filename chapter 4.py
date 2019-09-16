1543a1e1ca7680841eca33c5a2b15038294db229
jwcho95 <kyaleee@naver.com>
2019-09-16 15:40:56 +0900

    최종수정날짜: 2019/09/16

--- chapter 4.py
+++ chapter 4.py
@@ -85,3 +85,18 @@ dictionary["head"] = "새로운 정신"
 dictionary["body"] = "새로운 몸"
 
 print("요소 추가 이후:", dictionary)
+
+# 딕셔너리 내부에 키가 존재하는지 확인하고 값에 접근하기
+dictionary = {
+    "name" : "7D 건조 망고",
+    "type" : "당절임",
+    "ingredient" : ["망고","설탕","메타중아황산나트륨","치자황색소"],
+    "origin" : "필리핀"
+}
+
+key = input(">접근하고자 하는 키: ")
+
+if key in dictionary:
+    print(dictionary[key])
+else:
+    print("존재하지 않는 키에 접근하고 있습니다.")

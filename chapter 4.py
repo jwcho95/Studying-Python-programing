0fb18ab8b4a09cbc57fbe1f6405672c142b04aec
jwcho95 <kyaleee@naver.com>
2019-09-25 19:57:11 +0900

    최종수정날짜: 2019/09/25

--- chapter 4.py
+++ chapter 4.py
@@ -206,3 +206,17 @@ print()
 print("반복문과 조합하기")
 for i,value in enumerate(example_list):
     print("{}번째 요소는 {}입니다.".format(i, value))
+
+# 딕셔너리의 items() 함수와 반복문
+example_dictionary = {
+    "키A" : "값A",
+    "키B" : "값B",
+    "키C" : "값C"
+}
+
+print("items():", example_dictionary.items())
+print()
+
+print("딕셔너리의 items()함수와 반복문 조합하기")
+for key1, element in example_dictionary.items():
+    print("dictionary[{}] = {}".format(key1,element))

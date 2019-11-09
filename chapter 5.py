67caa87c9c3d000d80127d004930d570463dc0de
jwcho95 <kyaleee@naver.com>
2019-11-09 23:12:25 +0900

    최종수정날짜: 2019/11/09

--- chapter 5.py
+++ chapter 5.py
@@ -145,3 +145,13 @@ def test():
 a, b = test()
 print("a:", a)
 print("b:", b)
+
+# 함수의 매개 변수로 함수 전달하기
+def call_10_times(func):
+    for i in range(10):
+        func()
+
+def print_hello():
+    print("안녕하세요")
+
+call_10_times(print_hello)

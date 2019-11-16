e252fab41e7a9804fbe356fcfaf01c4bc08c11bd
jwcho95 <kyaleee@naver.com>
2019-11-16 23:38:07 +0900

    최종수정날짜: 2019/11/16

--- chapter 6.py
+++ chapter 6.py
@@ -21,3 +21,12 @@ if user_input_a.isdigit(): # isdigit()함수는 문자열이 숫자로 인식될
     print("원의 넓이:", 3.14 * number_input_a * number_input_a)
 else: # 이 부분이 이번 챕터에서 중요시 봐야하는 부분이다. 정수를 입력하지 않았을 경우까지 고려해주어야 프로그램이 오류가 나지 않는다.
     print("정수를 입력하지 않았습니다.")
+
+# try except 구문 : 예외가 발생할 가능성이 있는 코드를 try 구문 안에 넣고, 예외가 발생했을 시에 실행할 코드를 except 구문 안에 넣으면 된다.
+try:
+    number_input_b = int(input("정수 입력> "))
+    print("원의 반지름:", number_input_b)
+    print("원의 둘레:", 2 * 3.14 * number_input_b)
+    print("원의 넓이:", 3.14 * number_input_b * number_input_b)
+except:
+    print("무언가 잘못되었습니다.")
